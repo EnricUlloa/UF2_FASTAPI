824a7a98ca8c0838bc4b07ecefe9e5c80e7e3623
Enric <2024_enric.ulloa@iticbcn.cat>
2025-03-03 16:49:10 +0100

    Entrega2

--- ACTIVITAT_9/db_connect/database.py
+++ ACTIVITAT_9/db_connect/database.py
@@ -4,13 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# Cargar variables del archivo .env
 load_dotenv()
 
-# Obtener la URL de la base de datos desde el entorno
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Verificar que la URL no esté vacía
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL no está configurada en el archivo .env")
 
--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -6,7 +6,6 @@ from crud.user import read_users
 
 app = FastAPI()
 
-# Dependencia para la DB
 def get_db():
     db = SessionLocal()
     try:
@@ -14,7 +13,6 @@ def get_db():
     finally:
         db.close()
 
-# Modelo con Body-Fields
 class Item(BaseModel):
     name: str = Field(..., title="Item Name", max_length=50)
     description: str = Field(None, title="Description", max_length=200)
@@ -25,27 +23,27 @@ class Item(BaseModel):
 async def create_item(item: Item):
     return {"message": "Item received", "data": item}
 
-# Endpoint para obtener usuarios de la DB
+
 @app.get("/users/")
 def get_users(db=Depends(get_db)):
     return read_users(db)
 
-# Modelo anidado (submodelo)
+
 class Feature(BaseModel):
     name: str
     enabled: bool
 
-# Modelo principal con lista de submodelos
+
 class Product(BaseModel):
     name: str
     price: float
-    features: List[Feature]  # Lista de submodelos
-
+    features: List[Feature]  
+    
 @app.post("/products/")
 async def create_product(product: Product):
     return {"message": "Product received", "data": product}
 
-# Endpoint raíz
+
 @app.get("/")
 def read_root():
     return {"message": "Bienvenido a mi API con FastAPI"}
